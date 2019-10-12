fc0df2e9e06388320071ead2ecdab17f4a1d9134
livelybone <liuzhiping@auraxy.com>
2019-10-12 10:29:56 +0800

    feat(getItem): Update

--- src/form-items-manager.ts
+++ src/form-items-manager.ts
@@ -12,19 +12,17 @@ export class FormItemsManager<
     this.allItems = formItemsDictionary(formItems)
   }
 
-  getItem<Id extends keyof FormItems>(
-    id: Id,
-  ): (FormItems[Id] & { id: Id }) | undefined {
+  getItem<Id extends keyof FormItems>(id: Id): FormItems[Id] & { id: Id } {
     const item = this.allItems[id] as FormItems[Id] & { id: Id }
-    if (item) return item
-
-    console.warn(
-      `FormItemsManagement: The form item you search for by id \`${id}\` is not exist, please make sure param id correct`,
-    )
-    return undefined
+    if (!item) {
+      throw new Error(
+        `FormItemsManagement: The form item you search for by id \`${id}\` is not exist, please make sure param id correct`,
+      )
+    }
+    return item
   }
 
   getItems<Ids extends (keyof FormItems)[]>(ids: Ids) {
-    return ids.map(this.getItem.bind(this)).filter(Boolean)
+    return ids.map(this.getItem.bind(this))
   }
 }
